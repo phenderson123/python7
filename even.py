ad8b9ba6c712a10bf536ac425a06e5bad9a9b245
PranjalKhatri <68675142+PranjalKhatri@users.noreply.github.com>
2020-09-30 21:21:14 +0530

    Update even.py
    Added comments

--- even.py
+++ even.py
@@ -1,4 +1,8 @@
+
+#Take the number to evaluate
+
 num = int(input("Put in a number dude: "))
+#It it is divisible by 2 it means it is even else odd
 if (num % 2) == 0:
    print("{0} is Even".format(num))
 else:
